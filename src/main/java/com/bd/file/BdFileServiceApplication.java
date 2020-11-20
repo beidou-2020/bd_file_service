f5ddee26308c0d0fb4614a80802275229fa5dfcf
beidou <18513032646@163.com>
2020-11-20 10:16:20 +0800

    文件存储服务接入consul

--- src/main/java/com/bd/file/BdFileServiceApplication.java
+++ src/main/java/com/bd/file/BdFileServiceApplication.java
@@ -2,9 +2,11 @@ package com.bd.file;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 // 代表这是一个springboot应用。
 @SpringBootApplication(scanBasePackages = {"com.bd.file"})
+@EnableDiscoveryClient
 public class BdFileServiceApplication {
 
     /**
