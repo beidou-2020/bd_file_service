3101db90e6439dd01847a2b11a505988537377a3
beidou <18513032646@163.com>
2020-12-29 23:22:35 +0800

    文件存储服务隔离consul服务监控

--- src/main/java/com/bd/file/constant/FileConstant.java
+++ src/main/java/com/bd/file/constant/FileConstant.java
@@ -2,34 +2,35 @@ package com.bd.file.constant;
 
 import lombok.Data;
 import lombok.ToString;
-import org.springframework.boot.context.properties.ConfigurationProperties;
-import org.springframework.context.annotation.PropertySource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
 @Component
-@PropertySource(value = "classpath:application.properties")
-@ConfigurationProperties(prefix = "filepath")
 @Data
 @ToString
 public class FileConstant {
 
     /**
-     * 文件上传到文件服务器的存储路径
+     * 保存到文件服务器上的目的路径
      */
+    @Value("${filepath.uploadServerPath}")
     private String uploadServerPath;
-
-    /**
-     * 文件导出到客户端的本地存储路径
-     */
-    private String exportClientPath;
 
     /**
      * 文件服务器存储资源的绝对路径对应的虚拟路径(用于UploadFileConfig配置文件)
      */
+    @Value("${filepath.storageVirtualPath}")
     private String storageVirtualPath;
 
     /**
      * 文件服务器存储资源的绝对路径对应的虚拟路径(用于前端访问)
      */
+    @Value("${filepath.accessPath}")
     private String accessPath;
+
+    /**
+     * 文件导出到客户端的本地存储路径
+     */
+    /*@Value("${filepath.exportClientPath}")
+    private String exportClientPath;*/
 }
